af3c870c82d8b3346a59d19b8e37dc1cf2c62510
Mohammed Owes <Mohammed.Owes@rsa.com>
2021-05-31 21:02:32 +0530

    fixed josephus formula

--- C++/Sandbox/DSACourse/Recursion/Josephus.cpp
+++ C++/Sandbox/DSACourse/Recursion/Josephus.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int josephus(int n, int k) {
   if (n == 1)
     return n;
-  return (josephus(n - 1, k) + k - 1) % n;
+  return (josephus(n - 1, k) + k - 1) % n + 1;
 }
 
 int main() {
