1fc9fb2d1d77c5abb8513ac840fc12e4e8c4aedf
Mohammed Owes <Mohammed.Owes@rsa.com>
2021-05-31 20:55:13 +0530

    1. added josephus problem 2. added windows build and debug tasks 3. removed unnecessary files

---
+++ C++/Sandbox/DSACourse/Recursion/Josephus.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int josephus(int n, int k) {
+  if (n == 1)
+    return n;
+  return (josephus(n - 1, k) + k - 1) % n;
+}
+
+int main() {
+  int t;
+  scanf("%d", &t); // testcases
+  while (t--) {
+    int n, k;
+    scanf("%d%d", &n, &k); // taking input n and k
+
+    // calling josephus() function
+    printf("%d\n", josephus(n, k));
+  }
+  return 0;
+}
